a97039776bfbb0142cc16971b09c9405f3d6a26d
zendzo <zendzzo@gmail.com>
2018-12-01 17:17:02 +0700

    disabled registration link

--- resources/views/auth/login-v1/login.blade.php
+++ resources/views/auth/login-v1/login.blade.php
@@ -52,10 +52,10 @@
   </div>
 
   <div class="text-center p-t-136">
-    <a class="txt2" href="{{ route('register') }}">
+    {{-- <a class="txt2" href="{{ route('register') }}">
       Create your Account
       <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-    </a>
+    </a> --}}
   </div>
 </form>
 @endsection
